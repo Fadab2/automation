edaf3c54cfbdd7686476a5227aad26bdf4faa314
Fadab2 <ali_berg1@yahoo.com>
2022-02-20 19:59:53 -0800

    phone numbers format done

--- automation.py
+++ automation.py
@@ -17,24 +17,16 @@ emails_sorted = sorted(list(emails))
 phone_numbers_sorted = sorted(list(phone_numbers))
 
 # function to remove non digit characters.
-
-def format_phone(phone_list):
-  for number in phone_list:
-      clean_numbers = re.sub('[^0-9]', '', number)
-      if len(clean_numbers) == 10:
-        add_dashes = list(clean_numbers)
-        add_dashes.insert(3, '-')
-        add_dashes.insert(7, '-')
-
-        phone_list = ''.join(add_dashes)
-        print(phone_list)
-  return phone_list
-
-
+phone_list = ''
 with open ('phone_numbers.txt', 'w+') as f:
-  phone_numbers_sorted = format_phone(phone_numbers_sorted)
-  for line in phone_numbers_sorted:
-    f.write(f'{line}\n')
+  for number in phone_numbers_sorted:
+    clean_numbers = re.sub('[^0-9]', '', number)
+    if len(clean_numbers) == 10:
+      add_dashes = list(clean_numbers)
+      add_dashes.insert(3, '-')
+      add_dashes.insert(7, '-')
+      phone_list = ''.join(add_dashes)
+      f.write(f'{phone_list}\n')
 
 shutil.copy('phone_numbers.txt', 'assets')
 
